ebf44e2bd8c09816215ad9fe5a2e1bd0ace854bd
Piotr Gesicki <piotr.gesicki@gmail.com>
2024-03-04 20:59:43 +0100

    eibc corrupted memo test case

--- Makefile
+++ Makefile
@@ -20,8 +20,28 @@ e2e-test-eibc-fulfillment:
 e2e-test-ibc-grace-period:
 	cd tests && go test -timeout=25m -race -v -run TestIBCGracePeriodCompliance .
 
-# Executes all tests via rollup-e2e-testing
-e2e-test-all: e2e-test-ibc-success e2e-test-ibc-timeout e2e-test-ibc-grace-period e2e-test-eibc-fulfillment
+# Executes IBC tests via rollup-e2e-testing
+e2e-test-eibc-fulfill-no-balance:
+	cd tests && go test -timeout=25m -race -v -run TestEIBCFulfillOrderWithoutBalanceNegative .
 
-.PHONY: e2e-test-ibc-success e2e-test-ibc-timeout e2e-test-ibc-grace-period e2e-test-eibc-fulfillment e2e-test-all
+# Executes IBC tests via rollup-e2e-testing
+e2e-test-eibc-fulfill-more-than-once:	
+	cd tests && go test -timeout=25m -race -v -run TestEIBCFulfillOrderMoreThanOnceNegative .
+
+# Executes IBC tests via rollup-e2e-testing
+e2e-test-eibc-not-fulfilled:
+	cd tests && go test -timeout=25m -race -v -run TestEIBCNotFulfilled .
+
+# Executes IBC tests via rollup-e2e-testing
+e2e-test-eibc-corrupted-memo:
+	cd tests && go test -timeout=25m -race -v -run TestEIBCCorruptedMemoNegative .
+
+# Executes IBC tests via rollup-e2e-testing
+e2e-test-eibc-excessive-fee:
+	cd tests && go test -timeout=25m -race -v -run TestEIBCFeeMoreThanPacketAmountNegative .
+
+# Executes all tests via rollup-e2e-testing
+e2e-test-all: e2e-test-ibc-success e2e-test-ibc-timeout e2e-test-ibc-grace-period e2e-test-eibc-fulfillment e2e-test-eibc-fulfill-no-balance e2e-test-eibc-fulfill-more-than-once e2e-test-eibc-not-fulfilled e2e-test-eibc-corrupted-memo e2e-test-eibc-excessive-fee
+
+.PHONY: e2e-test-ibc-success e2e-test-ibc-timeout e2e-test-ibc-grace-period e2e-test-eibc-fulfillment e2e-test-eibc-fulfill-no-balance e2e-test-eibc-fulfill-more-than-once e2e-test-eibc-not-fulfilled e2e-test-eibc-corrupted-memo e2e-test-eibc-excessive-fee e2e-test-all
 
--- tests/eibc_demand_order_test.go
+++ tests/eibc_demand_order_test.go
@@ -259,6 +259,232 @@ func TestEIBCFulfillment(t *testing.T) {
 	)
 }
 
+// This test case verifies the system's behavior when an eIBC packet sent from the rollapp to the hub
+// that is fulfilled without having enough balance by the market maker
+func TestEIBCFulfillOrderWithoutBalanceNegative(t *testing.T) {
+
+}
+
+// This test case verifies the system's behavior when an eIBC packet is sent from the rollapp to the hub
+// that is fulfilled more than once
+func TestEIBCFulfillOrderMoreThanOnceNegative(t *testing.T) {
+
+}
+
+// This test case verifies the system's behavior when an eIBC packet sent from the rollapp to the hub
+// that is not fulfilled by a market maker
+func TestEIBCNotFulfilled(t *testing.T) {
+
+}
+
+// This test case verifies the system's behavior when an eIBC packet sent from the rollapp to the hub
+// that has incorrect memo data
+func TestEIBCCorruptedMemoNegative(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := context.Background()
+
+	configFileOverrides := make(map[string]any)
+	dymintTomlOverrides := make(testutil.Toml)
+	dymintTomlOverrides["settlement_layer"] = "dymension"
+	dymintTomlOverrides["node_address"] = fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
+	dymintTomlOverrides["rollapp_id"] = "demo-dymension-rollapp"
+
+	configFileOverrides["config/dymint.toml"] = dymintTomlOverrides
+	const BLOCK_FINALITY_PERIOD = 80
+	modifyGenesisKV := []cosmos.GenesisKV{
+		{
+			Key:   "app_state.rollapp.params.dispute_period_in_blocks",
+			Value: fmt.Sprint(BLOCK_FINALITY_PERIOD),
+		},
+	}
+
+	// Create chain factory with dymension
+	numHubVals := 1
+	numHubFullNodes := 1
+	numRollAppFn := 0
+	numRollAppVals := 1
+	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
+		{
+			Name: "rollapp1",
+			ChainConfig: ibc.ChainConfig{
+				Type:                "rollapp-dym",
+				Name:                "rollapp-temp",
+				ChainID:             "demo-dymension-rollapp",
+				Images:              []ibc.DockerImage{rollappImage},
+				Bin:                 "rollappd",
+				Bech32Prefix:        "rol",
+				Denom:               "urax",
+				CoinType:            "118",
+				GasPrices:           "0.0urax",
+				GasAdjustment:       1.1,
+				TrustingPeriod:      "112h",
+				NoHostMount:         false,
+				ModifyGenesis:       nil,
+				ConfigFileOverrides: configFileOverrides,
+			},
+			NumValidators: &numRollAppVals,
+			NumFullNodes:  &numRollAppFn,
+		},
+		{
+			Name: "dymension-hub",
+			ChainConfig: ibc.ChainConfig{
+				Type:                "hub-dym",
+				Name:                "dymension",
+				ChainID:             "dymension_100-1",
+				Images:              []ibc.DockerImage{dymensionImage},
+				Bin:                 "dymd",
+				Bech32Prefix:        "dym",
+				Denom:               "adym",
+				CoinType:            "118",
+				GasPrices:           "0.0adym",
+				EncodingConfig:      encodingConfig(),
+				GasAdjustment:       1.1,
+				TrustingPeriod:      "112h",
+				NoHostMount:         false,
+				ModifyGenesis:       cosmos.ModifyGenesis(modifyGenesisKV),
+				ConfigFileOverrides: nil,
+			},
+			NumValidators: &numHubVals,
+			NumFullNodes:  &numHubFullNodes,
+		},
+	})
+
+	// Get chains from the chain factory
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
+	dymension := chains[1].(*dym_hub.DymHub)
+
+	// Relayer Factory
+	client, network := test.DockerSetup(t)
+	r := test.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t),
+		relayer.CustomDockerImage("ghcr.io/cosmos/relayer", "reece-v2.3.1-ethermint", "100:1000"),
+	).Build(t, client, network)
+	const ibcPath = "ibc-path"
+	ic := test.NewSetup().
+		AddRollUp(dymension, rollapp1).
+		AddRelayer(r, "relayer").
+		AddLink(test.InterchainLink{
+			Chain1:  dymension,
+			Chain2:  rollapp1,
+			Relayer: r,
+			Path:    ibcPath,
+		})
+
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
+	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: false,
+
+		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
+		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
+	})
+	require.NoError(t, err)
+
+	walletAmount := math.NewInt(1_000_000_000_000)
+
+	// Create some user accounts on both chains
+	users := test.GetAndFundTestUsers(t, ctx, t.Name(), walletAmount, dymension, dymension, dymension, rollapp1)
+
+	// Wait a few blocks for relayer to start and for user accounts to be created
+	err = testutil.WaitForBlocks(ctx, 5, dymension, rollapp1)
+	require.NoError(t, err)
+
+	// Get our Bech32 encoded user addresses
+	dymensionUser, dymensionUser2, dymensionUser3, rollappUser := users[0], users[1], users[2], users[3]
+
+	dymensionUserAddr := dymensionUser.FormattedAddress()
+	dymensionUserAddr2 := dymensionUser2.FormattedAddress()
+	dymensionUserAddr3 := dymensionUser3.FormattedAddress()
+	rollappUserAddr := rollappUser.FormattedAddress()
+
+	// Assert the accounts were funded
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr, dymension.Config().Denom, walletAmount)
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr2, dymension.Config().Denom, walletAmount)
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr3, dymension.Config().Denom, walletAmount)
+	testutil.AssertBalance(t, ctx, rollapp1, rollappUserAddr, rollapp1.Config().Denom, walletAmount)
+
+	transferAmount := math.NewInt(1_000_000)
+
+	channel, err := ibc.GetTransferChannel(ctx, r, eRep, dymension.Config().ChainID, rollapp1.Config().ChainID)
+	require.NoError(t, err)
+
+	err = r.StartRelayer(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+
+	// Get the IBC denom for urax on Hub
+	rollappTokenDenom := transfertypes.GetPrefixedDenom(channel.Counterparty.PortID, channel.Counterparty.ChannelID, rollapp1.Config().Denom)
+	rollappIBCDenom := transfertypes.ParseDenomTrace(rollappTokenDenom).IBCDenom()
+	// end of preconditions
+
+	var options ibc.TransferOptions
+	// set eIBC specific memo
+	invalidMemo1 := `{"eibc": [{"fee": "200"}]}`
+	options.Memo = invalidMemo1
+	transferData := ibc.WalletData{
+		Address: dymensionUserAddr,
+		Denom:   rollapp1.Config().Denom,
+		Amount:  transferAmount,
+	}
+	_, err = rollapp1.SendIBCTransfer(ctx, channel.ChannelID, rollappUserAddr, transferData, options)
+	require.NoError(t, err)
+
+	invalidMemo2 := `{"eibc": {"feebaba": "200"}}`
+	options.Memo = invalidMemo2
+	transferData = ibc.WalletData{
+		Address: dymensionUserAddr2,
+		Denom:   rollapp1.Config().Denom,
+		Amount:  transferAmount,
+	}
+	_, err = rollapp1.SendIBCTransfer(ctx, channel.ChannelID, rollappUserAddr, transferData, options)
+	require.NoError(t, err)
+
+	invalidMemo3 := `{"eibc": {"fee": "this-should-be-number"}}`
+	options.Memo = invalidMemo3
+	transferData = ibc.WalletData{
+		Address: dymensionUserAddr3,
+		Denom:   rollapp1.Config().Denom,
+		Amount:  transferAmount,
+	}
+	_, err = rollapp1.SendIBCTransfer(ctx, channel.ChannelID, rollappUserAddr, transferData, options)
+	require.NoError(t, err)
+
+	// get eIbc events
+	eibcEvents, _ := getEIbcEventsWithinBlockRange(ctx, dymension, 30, false)
+	require.True(t, len(eibcEvents) == 0) // verify no EIBC event was registered on the hub
+
+	// we've waited 30 blocks already so we don't have to now wait the entire BLOCK_FINALITY_PERIOD blocks
+	err = testutil.WaitForBlocks(ctx, BLOCK_FINALITY_PERIOD-30+5, dymension)
+	require.NoError(t, err)
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr, rollappIBCDenom, transferData.Amount)
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr2, rollappIBCDenom, transferData.Amount)
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr3, rollappIBCDenom, transferData.Amount)
+
+	t.Cleanup(
+		func() {
+			err := r.StopRelayer(ctx, eRep)
+			if err != nil {
+				t.Logf("an error occurred while stopping the relayer: %s", err)
+			}
+		},
+	)
+
+}
+
+// This test case verifies the system's behavior when an eIBC packet sent from the rollapp to the hub
+// that has set fee more than a packet amount
+func TestEIBCFeeMoreThanPacketAmountNegative(t *testing.T) {
+
+}
+
 func getEibcEventFromTx(t *testing.T, dymension *dym_hub.DymHub, txhash string) *dymensiontesting.EibcEvent {
 	txResp, err := dymension.GetTransaction(txhash)
 	if err != nil {
