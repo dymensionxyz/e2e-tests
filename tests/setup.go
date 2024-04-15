268c52b6095f281e44dfb5910610106178420610
nguyen | decentrio <144610611+trinitys7@users.noreply.github.com>
2024-04-15 16:19:39 +0700

    fix: trigger genesis event (#90)

--- tests/setup.go
+++ tests/setup.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/decentrio/rollup-e2e-testing/cosmos"
 	"github.com/decentrio/rollup-e2e-testing/cosmos/hub/dym_hub"
+	"github.com/decentrio/rollup-e2e-testing/dymension"
 	"github.com/decentrio/rollup-e2e-testing/ibc"
 
 	hubgenesis "github.com/dymensionxyz/dymension-rdk/x/hub-genesis/types"
@@ -159,6 +160,15 @@ var (
 		{
 			Key:   "app_state.erc20.params.enable_evm_hook",
 			Value: false,
+		},
+		{
+			Key: "app_state.hubgenesis.state.genesis_tokens",
+			Value: []interface{}{
+				map[string]interface{}{
+					"denom":  "urax",
+					"amount": dymension.GenesisEventAmount.String(),
+				},
+			},
 		},
 	}
 
