79297495b39ce420b31252b5d9a9169e4d33acc7
lacsomot <hbnguyen.work@gmail.com>
2024-04-22 14:38:53 +0700

    add 3rd party eibc demand orders test case

--- Makefile
+++ Makefile
@@ -56,6 +56,9 @@ e2e-test-other-rollapp-not-affected-evm:  clean-e2e
 e2e-test-rollapp-genesis-event-evm: clean-e2e
 	cd tests && go test -timeout=25m -race -v -run TestRollappGenesisEvent_EVM .
 
+e2e-test-eibc-fulfillment-thirdparty-evm: clean-e2e
+	cd tests && go test -timeout=25m -race -v -run TestEIBCFulfillment_ThirdParty_EVM .
+
 # Executes IBC tests via rollup-e2e-testing
 e2e-test-ibc-success-wasm: clean-e2e
 	cd tests && go test -timeout=25m -race -v -run TestIBCTransferSuccess_Wasm .
--- tests/eibc_fulfillment_test.go
+++ tests/eibc_fulfillment_test.go
@@ -672,6 +672,393 @@ func TestEIBCFulfillment_Wasm(t *testing.T) {
 	)
 }
 
+// This test case verifies the system's behavior when an eIBC packet sent from the rollapp to the hub
+// that is fulfilled by the market maker
+func TestEIBCFulfillment_ThirdParty_EVM(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := context.Background()
+
+	// setup config for rollapp 1
+	settlement_layer_rollapp1 := "dymension"
+	node_address := fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
+	rollapp1_id := "rollappevm_1234-1"
+	gas_price_rollapp1 := "0adym"
+	emptyBlocksMaxTime := "3s"
+	configFileOverrides1 := overridesDymintToml(settlement_layer_rollapp1, node_address, rollapp1_id, gas_price_rollapp1, emptyBlocksMaxTime)
+
+	// setup config for rollapp 2
+	settlement_layer_rollapp2 := "dymension"
+	rollapp2_id := "rollappevm_12345-1"
+	gas_price_rollapp2 := "0adym"
+	configFileOverrides2 := overridesDymintToml(settlement_layer_rollapp2, node_address, rollapp2_id, gas_price_rollapp2, emptyBlocksMaxTime)
+
+	const BLOCK_FINALITY_PERIOD = 50
+	modifyGenesisKV := append(
+		dymensionGenesisKV,
+		cosmos.GenesisKV{
+			Key:   "app_state.rollapp.params.dispute_period_in_blocks",
+			Value: fmt.Sprint(BLOCK_FINALITY_PERIOD),
+		},
+	)
+
+	// Create chain factory with dymension
+	numHubVals := 1
+	numHubFullNodes := 1
+	numRollAppFn := 0
+	numRollAppVals := 1
+	numVals := 1
+	numFullNodes := 0
+	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
+		{
+			Name: "rollapp1",
+			ChainConfig: ibc.ChainConfig{
+				Type:                "rollapp-dym",
+				Name:                "rollapp-temp",
+				ChainID:             "rollappevm_1234-1",
+				Images:              []ibc.DockerImage{rollappEVMImage},
+				Bin:                 "rollappd",
+				Bech32Prefix:        "ethm",
+				Denom:               "urax",
+				CoinType:            "60",
+				GasPrices:           "0.0urax",
+				GasAdjustment:       1.1,
+				TrustingPeriod:      "112h",
+				EncodingConfig:      encodingConfig(),
+				NoHostMount:         false,
+				ModifyGenesis:       modifyRollappEVMGenesis(rollappEVMGenesisKV),
+				ConfigFileOverrides: configFileOverrides1,
+			},
+			NumValidators: &numRollAppVals,
+			NumFullNodes:  &numRollAppFn,
+		},
+		{
+			Name: "rollapp2",
+			ChainConfig: ibc.ChainConfig{
+				Type:                "rollapp-dym",
+				Name:                "rollapp-temp2",
+				ChainID:             "rollappevm_12345-1",
+				Images:              []ibc.DockerImage{rollappEVMImage},
+				Bin:                 "rollappd",
+				Bech32Prefix:        "ethm",
+				Denom:               "urax",
+				CoinType:            "60",
+				GasPrices:           "0.0urax",
+				GasAdjustment:       1.1,
+				TrustingPeriod:      "112h",
+				EncodingConfig:      encodingConfig(),
+				NoHostMount:         false,
+				ModifyGenesis:       modifyRollappEVMGenesis(rollappEVMGenesisKV),
+				ConfigFileOverrides: configFileOverrides2,
+			},
+			NumValidators: &numRollAppVals,
+			NumFullNodes:  &numRollAppFn,
+		},
+		{
+			Name: "dymension-hub",
+			ChainConfig: ibc.ChainConfig{
+				Type:                "hub-dym",
+				Name:                "dymension",
+				ChainID:             "dymension_100-1",
+				Images:              []ibc.DockerImage{dymensionImage},
+				Bin:                 "dymd",
+				Bech32Prefix:        "dym",
+				Denom:               "adym",
+				CoinType:            "60",
+				GasPrices:           "0.0adym",
+				EncodingConfig:      encodingConfig(),
+				GasAdjustment:       1.1,
+				TrustingPeriod:      "112h",
+				NoHostMount:         false,
+				ModifyGenesis:       modifyDymensionGenesis(modifyGenesisKV),
+				ConfigFileOverrides: nil,
+			},
+			NumValidators: &numHubVals,
+			NumFullNodes:  &numHubFullNodes,
+		},
+		{
+			Name:          "gaia",
+			Version:       "v14.2.0",
+			ChainConfig:   gaiaConfig,
+			NumValidators: &numVals,
+			NumFullNodes:  &numFullNodes,
+		},
+	})
+
+	// Get chains from the chain factory
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
+	rollapp2 := chains[1].(*dym_rollapp.DymRollApp)
+	dymension := chains[2].(*dym_hub.DymHub)
+	gaia := chains[3].(*cosmos.CosmosChain)
+
+	// Relayer Factory
+	client, network := test.DockerSetup(t)
+	// relayer for rollapp 1
+	r1 := test.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t),
+		relayer.CustomDockerImage("ghcr.io/decentrio/relayer", "e2e-amd", "100:1000"),
+	).Build(t, client, "relayer1", network)
+	// relayer for rollapp 2
+	r2 := test.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t),
+		relayer.CustomDockerImage("ghcr.io/decentrio/relayer", "e2e-amd", "100:1000"),
+	).Build(t, client, "relayer2", network)
+
+	r3 := test.NewBuiltinRelayerFactory(
+		ibc.CosmosRly,
+		zaptest.NewLogger(t),
+		relayer.CustomDockerImage(IBCRelayerImage, IBCRelayerVersion, "100:1000"),
+	).Build(t, client, "relayer3", network)
+
+	ic := test.NewSetup().
+		AddRollUp(dymension, rollapp1, rollapp2).
+		AddChain(gaia).
+		AddRelayer(r1, "relayer1").
+		AddRelayer(r2, "relayer2").
+		AddRelayer(r3, "relayer3").
+		AddLink(test.InterchainLink{
+			Chain1:  dymension,
+			Chain2:  rollapp1,
+			Relayer: r1,
+			Path:    ibcPath,
+		}).
+		AddLink(test.InterchainLink{
+			Chain1:  dymension,
+			Chain2:  rollapp2,
+			Relayer: r2,
+			Path:    ibcPath,
+		}).
+		AddLink(test.InterchainLink{
+			Chain1:  dymension,
+			Chain2:  gaia,
+			Relayer: r3,
+			Path:    ibcPath,
+		})
+
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
+	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: true,
+
+		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
+		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
+	})
+	require.NoError(t, err)
+
+	err = r1.GeneratePath(ctx, eRep, dymension.Config().ChainID, rollapp1.Config().ChainID, ibcPath)
+	require.NoError(t, err)
+	err = r2.GeneratePath(ctx, eRep, dymension.Config().ChainID, rollapp2.Config().ChainID, ibcPath)
+	require.NoError(t, err)
+	err = r3.GeneratePath(ctx, eRep, dymension.Config().ChainID, gaia.Config().ChainID, ibcPath)
+	require.NoError(t, err)
+
+	err = r1.CreateClients(ctx, eRep, ibcPath, ibc.DefaultClientOpts())
+	require.NoError(t, err)
+	err = r2.CreateClients(ctx, eRep, ibcPath, ibc.DefaultClientOpts())
+	require.NoError(t, err)
+	err = r3.CreateClients(ctx, eRep, ibcPath, ibc.DefaultClientOpts())
+	require.NoError(t, err)
+
+	err = testutil.WaitForBlocks(ctx, 30, dymension)
+	require.NoError(t, err)
+
+	r1.UpdateClients(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	r2.UpdateClients(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	r3.UpdateClients(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+
+	err = r1.CreateConnections(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	err = r2.CreateConnections(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	err = r3.CreateConnections(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+
+	err = testutil.WaitForBlocks(ctx, 10, dymension)
+	require.NoError(t, err)
+
+	err = r1.CreateChannel(ctx, eRep, ibcPath, ibc.DefaultChannelOpts())
+	require.NoError(t, err)
+	err = r2.CreateChannel(ctx, eRep, ibcPath, ibc.DefaultChannelOpts())
+	require.NoError(t, err)
+	err = r3.CreateChannel(ctx, eRep, ibcPath, ibc.DefaultChannelOpts())
+	require.NoError(t, err)
+
+	walletAmount := math.NewInt(1_000_000_000_000)
+
+	// Create some user accounts on both chains
+	users := test.GetAndFundTestUsers(t, ctx, t.Name(), walletAmount, dymension, dymension, gaia)
+
+	// Wait a few blocks for relayer to start and for user accounts to be created
+	err = testutil.WaitForBlocks(ctx, 5, dymension, rollapp1)
+	require.NoError(t, err)
+
+	// Get our Bech32 encoded user addresses
+	dymensionUser, marketMaker, gaiaUser := users[0], users[1], users[2]
+
+	dymensionUserAddr := dymensionUser.FormattedAddress()
+	marketMakerAddr := marketMaker.FormattedAddress()
+	gaiaUserAddr := gaiaUser.FormattedAddress()
+
+	// Assert the accounts were funded
+	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr, dymension.Config().Denom, walletAmount)
+	testutil.AssertBalance(t, ctx, dymension, marketMakerAddr, dymension.Config().Denom, walletAmount)
+	testutil.AssertBalance(t, ctx, gaia, gaiaUserAddr, gaia.Config().Denom, walletAmount)
+
+	transferAmount := math.NewInt(1_000_000)
+	multiplier := math.NewInt(10)
+
+	eibcFee := transferAmount.Quo(multiplier) // transferAmount * 0.1
+	transferAmountWithoutFee := transferAmount.Sub(eibcFee)
+
+	dymChannels, err := r1.GetChannels(ctx, eRep, dymension.Config().ChainID)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(dymChannels))
+
+	channsRollApp1, err := r1.GetChannels(ctx, eRep, rollapp1.GetChainID())
+	require.NoError(t, err)
+	require.Len(t, channsRollApp1, 1)
+
+	channsRollApp2, err := r2.GetChannels(ctx, eRep, rollapp2.GetChainID())
+	require.NoError(t, err)
+	require.Len(t, channsRollApp2, 1)
+
+	gaiaChannels, err := r3.GetChannels(ctx, eRep, gaia.GetChainID())
+	require.NoError(t, err)
+
+	require.Len(t, dymChannels, 3)
+	require.Len(t, gaiaChannels, 1)
+
+	channDymGaia := gaiaChannels[0].Counterparty
+	require.NotEmpty(t, channDymGaia.ChannelID)
+
+	channGaiaDym := gaiaChannels[0]
+	require.NotEmpty(t, channGaiaDym.ChannelID)
+
+	triggerHubGenesisEvent(t, dymension, rollappParam{
+		rollappID: rollapp1.Config().ChainID,
+		channelID: channsRollApp1[0].Counterparty.ChannelID,
+		userKey:   dymensionUser.KeyName(),
+	})
+
+	triggerHubGenesisEvent(t, dymension, rollappParam{
+		rollappID: rollapp2.Config().ChainID,
+		channelID: channsRollApp2[0].Counterparty.ChannelID,
+		userKey:   dymensionUser.KeyName(),
+	})
+
+	// Start relayer
+	err = r1.StartRelayer(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	err = r2.StartRelayer(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+	err = r3.StartRelayer(ctx, eRep, ibcPath)
+	require.NoError(t, err)
+
+	err = testutil.WaitForBlocks(ctx, 5, dymension)
+	require.NoError(t, err)
+
+	transferData := ibc.WalletData{
+		Address: marketMakerAddr,
+		Denom:   gaia.Config().Denom,
+		Amount:  transferAmount,
+	}
+
+	// Get the IBC denom for urax on Hub
+	gaiaTokenDenom := transfertypes.GetPrefixedDenom(channDymGaia.PortID, channDymGaia.ChannelID, gaia.Config().Denom)
+	gaiaIBCDenom := transfertypes.ParseDenomTrace(gaiaTokenDenom).IBCDenom()
+
+	var options ibc.TransferOptions
+	// market maker needs to have funds on the hub first to be able to fulfill upcoming demand order
+	_, err = gaia.SendIBCTransfer(ctx, channGaiaDym.ChannelID, gaiaUserAddr, transferData, options)
+	require.NoError(t, err)
+
+	t.Log("gaiaIBCDenom:", gaiaIBCDenom)
+
+	expMmBalanceGaiaDenom := transferData.Amount
+	balance, err := dymension.GetBalance(ctx, marketMakerAddr, gaiaIBCDenom)
+	require.NoError(t, err)
+	fmt.Println("Balance of marketMakerAddr after preconditions:", balance)
+	require.True(t, balance.Equal(expMmBalanceGaiaDenom), fmt.Sprintf("Value mismatch. Expected %s, actual %s", expMmBalanceGaiaDenom, balance))
+	// end of preconditions
+
+	transferData = ibc.WalletData{
+		Address: dymensionUserAddr,
+		Denom:   gaia.Config().Denom,
+		Amount:  transferAmount,
+	}
+
+	// set eIBC specific memo
+	options.Memo = BuildEIbcMemo(eibcFee)
+
+	_, err = gaia.SendIBCTransfer(ctx, channGaiaDym.ChannelID, gaiaUserAddr, transferData, options)
+	require.NoError(t, err)
+
+	balance, err = dymension.GetBalance(ctx, dymensionUserAddr, gaiaIBCDenom)
+	require.NoError(t, err)
+	fmt.Println("Balance of dymensionUserAddr right after sending eIBC transfer:", balance)
+	require.True(t, balance.Equal(transferAmount), fmt.Sprintf("Value mismatch. Expected %s, actual %s", transferAmount, balance))
+
+	// get eIbc event
+	eibcEvents, err := getEIbcEventsWithinBlockRange(ctx, dymension, 30, false)
+	require.NoError(t, err)
+	fmt.Println("Event:", eibcEvents[0])
+
+	// fulfill demand order
+	txhash, err := dymension.FullfillDemandOrder(ctx, eibcEvents[0].ID, marketMakerAddr)
+	require.NoError(t, err)
+	fmt.Println(txhash)
+	eibcEvent := getEibcEventFromTx(t, dymension, txhash)
+	if eibcEvent != nil {
+		fmt.Println("After order fulfillment:", eibcEvent)
+	}
+
+	// wait a few blocks and verify sender received funds on the hub
+	err = testutil.WaitForBlocks(ctx, 5, dymension)
+	require.NoError(t, err)
+
+	// verify funds minus fee were added to receiver's address
+	balance, err = dymension.GetBalance(ctx, dymensionUserAddr, gaiaIBCDenom)
+	require.NoError(t, err)
+	fmt.Println("Balance of dymensionUserAddr after fulfilling the order:", balance)
+	require.True(t, balance.Equal(transferAmountWithoutFee), fmt.Sprintf("Value mismatch. Expected %s, actual %s", transferAmountWithoutFee, balance))
+
+	// verify funds were deducted from market maker's wallet address
+	balance, err = dymension.GetBalance(ctx, marketMakerAddr, gaiaIBCDenom)
+	require.NoError(t, err)
+	fmt.Println("Balance of marketMakerAddr after fulfilling the order:", balance)
+	expMmBalanceGaiaDenom = expMmBalanceGaiaDenom.Sub((transferAmountWithoutFee))
+	require.True(t, balance.Equal(expMmBalanceGaiaDenom), fmt.Sprintf("Value mismatch. Expected %s, actual %s", expMmBalanceGaiaDenom, balance))
+
+	// t.Cleanup(
+	// 	func() {
+	// 		err := r1.StopRelayer(ctx, eRep)
+	// 		if err != nil {
+	// 			t.Logf("an error occurred while stopping the relayer: %s", err)
+	// 		}
+
+	// 		err = r2.StopRelayer(ctx, eRep)
+	// 		if err != nil {
+	// 			t.Logf("an error occurred while stopping the relayer: %s", err)
+	// 		}
+
+	// 		err = r3.StopRelayer(ctx, eRep)
+	// 		if err != nil {
+	// 			t.Logf("an error occurred while stopping the relayer: %s", err)
+	// 		}
+	// 	},
+	// )
+}
+
 func getEibcEventFromTx(t *testing.T, dymension *dym_hub.DymHub, txhash string) *dymensiontesting.EibcEvent {
 	txResp, err := dymension.GetTransaction(txhash)
 	if err != nil {
