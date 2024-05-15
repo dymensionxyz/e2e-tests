c207a3e5da29e077d1d07fe33522dbd8ec5076ad
ducnt131 <62016666+anhductn2001@users.noreply.github.com>
2024-05-15 13:37:13 +0700

    feat!: get all the test cases and add them to the CI matrix (#152)

---
+++ run-e2e.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+TEST="${1}"
+
+# run the test file directly, this allows log output to be streamed directly in the terminal sessions
+# without needed to wait for the test to finish.
+# it shouldn't take 30m, but the wasm test can be quite slow, so we can be generous.
+cd tests && go test -timeout=25m -race -v -run ${TEST} .
