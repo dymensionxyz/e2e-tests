af8ababfa72a8385f81cf173e3f97696a54ec5f4
anhductn2001 <ntd1312000@gmail.com>
2024-06-05 10:34:42 +0700

    update image post-upgrade

--- tests/rollapp_upgrade_test.go
+++ tests/rollapp_upgrade_test.go
@@ -153,7 +153,7 @@ func TestRollappUpgradeNonStateBreaking_EVM(t *testing.T) {
 	err = rollapp1.StopAllNodes(ctx)
 	require.NoError(t, err, "error stopping node(s)")
 
-	rollapp1.UpgradeVersion(ctx, client, "ghcr.io/decentrio/rollapp-evm", "post-upgrade-non-state-breaking")
+	rollapp1.UpgradeVersion(ctx, client, RollappEVMMainRepo, rollappEVMVersion)
 
 	err = rollapp1.StartAllNodes(ctx)
 	require.NoError(t, err, "error starting node(s)")
