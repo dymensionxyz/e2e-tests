af616f27b27b2fa36b548a8150854400f42ce3cc
ducnt131 <62016666+anhductn2001@users.noreply.github.com>
2024-06-07 15:55:05 +0700

    feat: setup e2e-live-tests (#237)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ live-tests/ibc_transfer_test.go
@@ -0,0 +1,171 @@
+package livetests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"cosmossdk.io/math"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	"github.com/decentrio/e2e-testing-live/cosmos"
+	"github.com/decentrio/e2e-testing-live/testutil"
+	"github.com/decentrio/rollup-e2e-testing/ibc"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIBCTransfer_Live(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	ctx := context.Background()
+
+	hub := cosmos.CosmosChain{
+		RPCAddr:       "rpc-blumbus.mzonder.com:443",
+		GrpcAddr:      "grpc-blumbus.mzonder.com:9090",
+		ChainID:       "blumbus_111-1",
+		Bin:           "dymd",
+		GasPrices:     "1000adym",
+		GasAdjustment: "1.1",
+		Denom:         "adym",
+	}
+
+	rollappX := cosmos.CosmosChain{
+		RPCAddr:       "rpc.rolxtwo.evm.ra.blumbus.noisnemyd.xyz:443",
+		GrpcAddr:      "3.123.185.77:9090",
+		ChainID:       "rolx_100004-1",
+		Bin:           "rollapp-evm",
+		GasPrices:     "0.0arolx",
+		GasAdjustment: "1.1",
+		Denom:         "arolx",
+	}
+
+	rollappY := cosmos.CosmosChain{
+		RPCAddr:       "rpc.roly.wasm.ra.blumbus.noisnemyd.xyz:443",
+		GrpcAddr:      "18.153.150.111:9090",
+		ChainID:       "rollappy_700002-1",
+		Bin:           "rollapp-wasm",
+		GasPrices:     "0.0aroly",
+		GasAdjustment: "1.1",
+		Denom:         "aroly",
+	}
+
+	dymensionUser, err := hub.CreateUser("dym1")
+	require.NoError(t, err)
+	rollappXUser, err := rollappX.CreateUser("rolx1")
+	require.NoError(t, err)
+	rollappYUser, err := rollappY.CreateUser("roly1")
+	require.NoError(t, err)
+
+	err = hub.NewClient("https://" + hub.RPCAddr)
+	require.NoError(t, err)
+
+	err = rollappX.NewClient("https://" + rollappX.RPCAddr)
+	require.NoError(t, err)
+
+	err = rollappY.NewClient("https://" + rollappY.RPCAddr)
+	require.NoError(t, err)
+
+	dymensionUser.GetFaucet("http://18.184.170.181:3000/api/get-dym")
+	rollappXUser.GetFaucet("http://18.184.170.181:3000/api/get-rollx")
+	rollappYUser.GetFaucet("http://18.184.170.181:3000/api/get-rolly")
+
+	// Wait for blocks
+	testutil.WaitForBlocks(ctx, 5, hub)
+
+	// Get the IBC denom
+	rollappXTokenDenom := transfertypes.GetPrefixedDenom("transfer", channelIDDymRollappX, rollappXUser.Denom)
+	rollappXIBCDenom := transfertypes.ParseDenomTrace(rollappXTokenDenom).IBCDenom()
+
+	rollappYTokenDenom := transfertypes.GetPrefixedDenom("transfer", channelIDDymRollappY, rollappYUser.Denom)
+	rollappYIBCDenom := transfertypes.ParseDenomTrace(rollappYTokenDenom).IBCDenom()
+
+	hubTokenDenom := transfertypes.GetPrefixedDenom("transfer", channelIDRollappXDym, dymensionUser.Denom)
+	hubIBCDenom := transfertypes.ParseDenomTrace(hubTokenDenom).IBCDenom()
+
+	dymensionOrigBal, err := dymensionUser.GetBalance(ctx, dymensionUser.Denom, hub.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(dymensionOrigBal)
+
+	rollappXOrigBal, err := rollappXUser.GetBalance(ctx, rollappXUser.Denom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(rollappXOrigBal)
+
+	rollappYOrigBal, err := rollappYUser.GetBalance(ctx, rollappYUser.Denom, rollappY.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(rollappYOrigBal)
+
+	erc20_OrigBal, err := GetERC20Balance(ctx, erc20IBCDenom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(erc20_OrigBal)
+
+	// Compose an IBC transfer and send from dymension -> rollapp
+	transferData := ibc.WalletData{
+		Address: rollappXUser.Address,
+		Denom:   dymensionUser.Denom,
+		Amount:  transferAmount,
+	}
+
+	testutil.WaitForBlocks(ctx, 3, hub)
+
+	cosmos.SendIBCTransfer(hub, channelIDDymRollappX, dymensionUser.Address, transferData, dymFee, ibc.TransferOptions{})
+	require.NoError(t, err)
+
+	testutil.WaitForBlocks(ctx, 3, hub)
+	// Compose an IBC transfer and send from rollapp -> hub
+	transferData = ibc.WalletData{
+		Address: dymensionUser.Address,
+		Denom:   rollappXUser.Denom,
+		Amount:  transferAmount,
+	}
+
+	multiplier := math.NewInt(10)
+	eibcFee := transferAmount.Quo(multiplier) // transferAmount * 0.1
+
+	var options ibc.TransferOptions
+	// set eIBC specific memo
+	options.Memo = BuildEIbcMemo(eibcFee)
+	cosmos.SendIBCTransfer(rollappX, channelIDRollappXDym, rollappXUser.Address, transferData, rolxFee, options)
+	require.NoError(t, err)
+
+	testutil.WaitForBlocks(ctx, 10, hub)
+
+	erc20_Bal, err := GetERC20Balance(ctx, hubIBCDenom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(erc20_Bal)
+	fmt.Println(rollappXIBCDenom)
+
+	testutil.AssertBalance(t, ctx, dymensionUser, rollappXIBCDenom, hub.GrpcAddr, transferAmount.Sub(eibcFee))
+	require.Equal(t, erc20_OrigBal.Add(transferAmount), erc20_Bal)
+
+	// Compose an IBC transfer and send from dymension -> rollapp
+	transferData = ibc.WalletData{
+		Address: rollappYUser.Address,
+		Denom:   dymensionUser.Denom,
+		Amount:  transferAmount,
+	}
+
+	cosmos.SendIBCTransfer(hub, channelIDDymRollappY, dymensionUser.Address, transferData, dymFee, ibc.TransferOptions{})
+	require.NoError(t, err)
+
+	testutil.WaitForBlocks(ctx, 3, hub)
+
+	// Compose an IBC transfer and send from rollapp -> hub
+	transferData = ibc.WalletData{
+		Address: dymensionUser.Address,
+		Denom:   rollappYUser.Denom,
+		Amount:  transferAmount,
+	}
+
+	cosmos.SendIBCTransfer(rollappY, channelIDRollappYDym, rollappYUser.Address, transferData, rolyFee, options)
+	require.NoError(t, err)
+
+	testutil.WaitForBlocks(ctx, 10, hub)
+
+	erc20_Bal, err = GetERC20Balance(ctx, hubIBCDenom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println(erc20_Bal)
+	fmt.Println(rollappYIBCDenom)
+
+	testutil.AssertBalance(t, ctx, dymensionUser, rollappYIBCDenom, hub.GrpcAddr, transferAmount.Sub(eibcFee))
+	require.Equal(t, erc20_OrigBal.Add(transferAmount), erc20_Bal)
+}
