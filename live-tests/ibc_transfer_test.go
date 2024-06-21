cc27c6973d7b3ddf132898c75ffacc6fa141b0b6
hoangdv2429 <hoangdv2429@gmail.com>
2024-06-21 15:52:54 +0700

    need to assert status of rollapp state

--- live-tests/ibc_transfer_test.go
+++ live-tests/ibc_transfer_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"cosmossdk.io/math"
+	sdkmath "cosmossdk.io/math"
 	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	"github.com/decentrio/e2e-testing-live/cosmos"
 	"github.com/decentrio/e2e-testing-live/testutil"
@@ -184,4 +185,109 @@ func TestIBCTransfer_Live(t *testing.T) {
 	// rolly currently don't support eibc
 	testutil.AssertBalance(t, ctx, rollappYUser, hubIBCDenom, rollappY.GrpcAddr, transferAmount)
 	testutil.AssertBalance(t, ctx, dymensionUser, rollappYIBCDenom, hub.GrpcAddr, transferAmount)
+}
+
+// TestDelayackRollappToHubNoFinalized_Live test IBC transfer from rollapp to hub, so that its succeeds (creates rollapp packet) when rollapp has NO FINALIZED STATES AT ALL (just pending)
+func TestDelayackRollappToHubNoFinalized_Live(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	ctx := context.Background()
+
+	hub := cosmos.CosmosChain{
+		RPCAddr:       "rpc-blumbus.mzonder.com:443",
+		GrpcAddr:      "grpc-blumbus.mzonder.com:9090",
+		ChainID:       "blumbus_111-1",
+		Bin:           "dymd",
+		GasPrices:     "1000adym",
+		GasAdjustment: "1.1",
+		Denom:         "adym",
+	}
+
+	rollappX := cosmos.CosmosChain{
+		RPCAddr:       "rpc.rolxtwo.evm.ra.blumbus.noisnemyd.xyz:443",
+		GrpcAddr:      "3.123.185.77:9090",
+		ChainID:       "rolx_100004-1",
+		Bin:           "rollapp-evm",
+		GasPrices:     "0.0arolx",
+		GasAdjustment: "1.1",
+		Denom:         "arolx",
+	}
+
+	rollappY := cosmos.CosmosChain{
+		RPCAddr:       "rpc.roly.wasm.ra.blumbus.noisnemyd.xyz:443",
+		GrpcAddr:      "18.153.150.111:9090",
+		ChainID:       "rollappy_700002-1",
+		Bin:           "rollapp-wasm",
+		GasPrices:     "0.0aroly",
+		GasAdjustment: "1.1",
+		Denom:         "aroly",
+	}
+
+	dymensionUser, err := hub.CreateUser("dym1")
+	require.NoError(t, err)
+	rollappXUser, err := rollappX.CreateUser("rolx1")
+	require.NoError(t, err)
+	rollappYUser, err := rollappY.CreateUser("roly1")
+	require.NoError(t, err)
+
+	err = hub.NewClient("https://" + hub.RPCAddr)
+	require.NoError(t, err)
+
+	err = rollappX.NewClient("https://" + rollappX.RPCAddr)
+	require.NoError(t, err)
+
+	err = rollappY.NewClient("https://" + rollappY.RPCAddr)
+	require.NoError(t, err)
+
+	dymensionUser.GetFaucet("http://18.184.170.181:3000/api/get-dym")
+	rollappXUser.GetFaucet("http://18.184.170.181:3000/api/get-rollx")
+	rollappYUser.GetFaucet("http://18.184.170.181:3000/api/get-rolly")
+
+	// Wait for blocks
+	testutil.WaitForBlocks(ctx, 5, hub)
+
+	// Get the IBC denom
+	rollappXTokenDenom := transfertypes.GetPrefixedDenom("transfer", channelIDDymRollappX, rollappXUser.Denom)
+	rollappXIBCDenom := transfertypes.ParseDenomTrace(rollappXTokenDenom).IBCDenom()
+
+	dymensionOrigBal, err := dymensionUser.GetBalance(ctx, dymensionUser.Denom, hub.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println("hub original balance: ", dymensionOrigBal, dymensionUser.Denom)
+
+	rollappXOrigBal, err := rollappXUser.GetBalance(ctx, rollappXUser.Denom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println("rollapp x original balance: ", rollappXOrigBal, rollappXUser.Denom)
+
+	rollappYOrigBal, err := rollappYUser.GetBalance(ctx, rollappYUser.Denom, rollappY.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println("rollapp y original balance: ", rollappYOrigBal, rollappYUser.Denom)
+
+	erc20_OrigBal, err := GetERC20Balance(ctx, erc20IBCDenom, rollappX.GrpcAddr)
+	require.NoError(t, err)
+	fmt.Println("rol x user balance of denom send from dym: ", erc20_OrigBal)
+
+	// Compose an IBC transfer and send from rollapp -> hub
+	transferData := ibc.WalletData{
+		Address: dymensionUser.Address,
+		Denom:   rollappXUser.Denom,
+		Amount:  transferAmount,
+	}
+
+	var options ibc.TransferOptions
+	cosmos.SendIBCTransfer(rollappX, channelIDRollappXDym, rollappXUser.Address, transferData, rolxFee, options)
+	require.NoError(t, err)
+	// Amount should not be received by hub yet
+	testutil.AssertBalance(t, ctx, dymensionUser, rollappXIBCDenom, hub.GrpcAddr, sdkmath.NewInt(0))
+
+	// wait for hub to finalize
+	testutil.WaitForBlocks(ctx, disputed_period_plus_batch_submit_blocks, hub)
+
+	// TODO: sub bridging fee on new version
+	testutil.AssertBalance(t, ctx, dymensionUser, rollappXIBCDenom, hub.GrpcAddr, transferAmount)
+
+	// Check rollapp is not yet have finalized state
+	res, err := cosmos.QueryRollappState(hub, rollappX.ChainID, false)
+	require.NoError(t, err)
+	fmt.Println("rollapp state: ", res)
 }
