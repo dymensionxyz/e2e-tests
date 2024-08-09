d177adf0468911610455c3f74f8bec9511643a2c
anhductn2001 <ntd1312000@gmail.com>
2024-08-09 10:18:37 +0700

    update

--- tests/fullnode_sync_test.go
+++ tests/fullnode_sync_test.go
@@ -53,246 +53,246 @@ func StartDA() {
 	}
 }
 
-func TestFullnodeSync_EVM(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
+// func TestFullnodeSync_EVM(t *testing.T) {
+// 	if testing.Short() {
+// 		t.Skip()
+// 	}
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	go StartDA()
+// 	go StartDA()
 
-	// setup config for rollapp 1
-	settlement_layer_rollapp1 := "dymension"
-	settlement_node_address := fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
-	rollapp1_id := "rollappevm_1234-1"
-	gas_price_rollapp1 := "0adym"
-	maxIdleTime1 := "5s"
-	maxProofTime := "500ms"
-	configFileOverrides := overridesDymintToml(settlement_layer_rollapp1, settlement_node_address, rollapp1_id, gas_price_rollapp1, maxIdleTime1, maxProofTime, "20s")
+// 	// setup config for rollapp 1
+// 	settlement_layer_rollapp1 := "dymension"
+// 	settlement_node_address := fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
+// 	rollapp1_id := "rollappevm_1234-1"
+// 	gas_price_rollapp1 := "0adym"
+// 	maxIdleTime1 := "5s"
+// 	maxProofTime := "500ms"
+// 	configFileOverrides := overridesDymintToml(settlement_layer_rollapp1, settlement_node_address, rollapp1_id, gas_price_rollapp1, maxIdleTime1, maxProofTime, "20s")
 
-	// Create chain factory with dymension
-	numHubVals := 1
-	numHubFullNodes := 1
-	numRollAppVals := 1
-	numRollAppFn := 1
+// 	// Create chain factory with dymension
+// 	numHubVals := 1
+// 	numHubFullNodes := 1
+// 	numRollAppVals := 1
+// 	numRollAppFn := 1
 
-	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
-		{
-			Name: "rollapp1",
-			ChainConfig: ibc.ChainConfig{
-				Type:                "rollapp-dym",
-				Name:                "rollapp-temp",
-				ChainID:             "rollappevm_1234-1",
-				Images:              []ibc.DockerImage{rollappEVMImage},
-				Bin:                 "rollappd",
-				Bech32Prefix:        "ethm",
-				Denom:               "urax",
-				CoinType:            "60",
-				GasPrices:           "0.0urax",
-				GasAdjustment:       1.1,
-				TrustingPeriod:      "112h",
-				EncodingConfig:      encodingConfig(),
-				NoHostMount:         false,
-				ModifyGenesis:       modifyRollappEVMGenesis(rollappEVMGenesisKV),
-				ConfigFileOverrides: configFileOverrides,
-			},
-			NumValidators: &numRollAppVals,
-			NumFullNodes:  &numRollAppFn,
-		},
-		{
-			Name:          "dymension-hub",
-			ChainConfig:   customEpochConfig("5s"),
-			NumValidators: &numHubVals,
-			NumFullNodes:  &numHubFullNodes,
-		},
-	})
+// 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
+// 		{
+// 			Name: "rollapp1",
+// 			ChainConfig: ibc.ChainConfig{
+// 				Type:                "rollapp-dym",
+// 				Name:                "rollapp-temp",
+// 				ChainID:             "rollappevm_1234-1",
+// 				Images:              []ibc.DockerImage{rollappEVMImage},
+// 				Bin:                 "rollappd",
+// 				Bech32Prefix:        "ethm",
+// 				Denom:               "urax",
+// 				CoinType:            "60",
+// 				GasPrices:           "0.0urax",
+// 				GasAdjustment:       1.1,
+// 				TrustingPeriod:      "112h",
+// 				EncodingConfig:      encodingConfig(),
+// 				NoHostMount:         false,
+// 				ModifyGenesis:       modifyRollappEVMGenesis(rollappEVMGenesisKV),
+// 				ConfigFileOverrides: configFileOverrides,
+// 			},
+// 			NumValidators: &numRollAppVals,
+// 			NumFullNodes:  &numRollAppFn,
+// 		},
+// 		{
+// 			Name:          "dymension-hub",
+// 			ChainConfig:   customEpochConfig("5s"),
+// 			NumValidators: &numHubVals,
+// 			NumFullNodes:  &numHubFullNodes,
+// 		},
+// 	})
 
-	// Get chains from the chain factory
-	chains, err := cf.Chains(t.Name())
-	require.NoError(t, err)
+// 	// Get chains from the chain factory
+// 	chains, err := cf.Chains(t.Name())
+// 	require.NoError(t, err)
 
-	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
-	dymension := chains[1].(*dym_hub.DymHub)
+// 	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
+// 	dymension := chains[1].(*dym_hub.DymHub)
 
-	// Relayer Factory
-	client, network := test.DockerSetup(t)
+// 	// Relayer Factory
+// 	client, network := test.DockerSetup(t)
 
-	ic := test.NewSetup().
-		AddRollUp(dymension, rollapp1)
+// 	ic := test.NewSetup().
+// 		AddRollUp(dymension, rollapp1)
 
-	rep := testreporter.NewNopReporter()
-	eRep := rep.RelayerExecReporter(t)
+// 	rep := testreporter.NewNopReporter()
+// 	eRep := rep.RelayerExecReporter(t)
 
-	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
-		TestName:         t.Name(),
-		Client:           client,
-		NetworkID:        network,
-		SkipPathCreation: true,
+// 	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
+// 		TestName:         t.Name(),
+// 		Client:           client,
+// 		NetworkID:        network,
+// 		SkipPathCreation: true,
 
-		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
-		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
-	}, nil, "", nil)
-	require.NoError(t, err)
+// 		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
+// 		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
+// 	}, nil, "", nil)
+// 	require.NoError(t, err)
 
-	// Wait for rollapp finalized
-	rollapp1Height, err := rollapp1.Validators[0].Height(ctx)
-	require.NoError(t, err)
-	isFinalized, err := dymension.WaitUntilRollappHeightIsFinalized(ctx, rollapp1.GetChainID(), rollapp1Height, 300)
-	require.True(t, isFinalized)
-	require.NoError(t, err)
+// 	// Wait for rollapp finalized
+// 	rollapp1Height, err := rollapp1.Validators[0].Height(ctx)
+// 	require.NoError(t, err)
+// 	isFinalized, err := dymension.WaitUntilRollappHeightIsFinalized(ctx, rollapp1.GetChainID(), rollapp1Height, 300)
+// 	require.True(t, isFinalized)
+// 	require.NoError(t, err)
 
-	// Stop the full node
-	err = rollapp1.FullNodes[0].StopContainer(ctx)
-	require.NoError(t, err)
+// 	// Stop the full node
+// 	err = rollapp1.FullNodes[0].StopContainer(ctx)
+// 	require.NoError(t, err)
 
-	// Wait for a few blocks before start the node again and sync
-	err = testutil.WaitForBlocks(ctx, 50, dymension)
-	require.NoError(t, err)
+// 	// Wait for a few blocks before start the node again and sync
+// 	err = testutil.WaitForBlocks(ctx, 50, dymension)
+// 	require.NoError(t, err)
 
-	// Start full node again
-	err = rollapp1.FullNodes[0].StartContainer(ctx)
-	require.NoError(t, err)
+// 	// Start full node again
+// 	err = rollapp1.FullNodes[0].StartContainer(ctx)
+// 	require.NoError(t, err)
 
-	// Poll until full node is sync
-	err = testutil.WaitForCondition(
-		time.Minute*50,
-		time.Second*5, // each epoch is 5 seconds
-		func() (bool, error) {
-			valHeight, err := rollapp1.Validators[0].Height(ctx)
-			require.NoError(t, err)
+// 	// Poll until full node is sync
+// 	err = testutil.WaitForCondition(
+// 		time.Minute*50,
+// 		time.Second*5, // each epoch is 5 seconds
+// 		func() (bool, error) {
+// 			valHeight, err := rollapp1.Validators[0].Height(ctx)
+// 			require.NoError(t, err)
 
-			fullnodeHeight, err := rollapp1.FullNodes[0].Height(ctx)
-			require.NoError(t, err)
+// 			fullnodeHeight, err := rollapp1.FullNodes[0].Height(ctx)
+// 			require.NoError(t, err)
 
-			fmt.Println("valHeight", valHeight, " || fullnodeHeight", fullnodeHeight)
-			if valHeight > fullnodeHeight {
-				return false, nil
-			}
+// 			fmt.Println("valHeight", valHeight, " || fullnodeHeight", fullnodeHeight)
+// 			if valHeight > fullnodeHeight {
+// 				return false, nil
+// 			}
 
-			return true, nil
-		},
-	)
-	require.NoError(t, err)
-}
+// 			return true, nil
+// 		},
+// 	)
+// 	require.NoError(t, err)
+// }
 
-func TestFullnodeSync_Wasm(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
+// func TestFullnodeSync_Wasm(t *testing.T) {
+// 	if testing.Short() {
+// 		t.Skip()
+// 	}
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	configFileOverrides := make(map[string]any)
-	dymintTomlOverrides := make(testutil.Toml)
-	dymintTomlOverrides["settlement_layer"] = "dymension"
-	dymintTomlOverrides["node_address"] = fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
-	dymintTomlOverrides["rollapp_id"] = "rollappwasm_1234-1"
-	dymintTomlOverrides["gas_prices"] = "0adym"
-	dymintTomlOverrides["empty_blocks_max_time"] = "3s"
+// 	configFileOverrides := make(map[string]any)
+// 	dymintTomlOverrides := make(testutil.Toml)
+// 	dymintTomlOverrides["settlement_layer"] = "dymension"
+// 	dymintTomlOverrides["node_address"] = fmt.Sprintf("http://dymension_100-1-val-0-%s:26657", t.Name())
+// 	dymintTomlOverrides["rollapp_id"] = "rollappwasm_1234-1"
+// 	dymintTomlOverrides["gas_prices"] = "0adym"
+// 	dymintTomlOverrides["empty_blocks_max_time"] = "3s"
 
-	configFileOverrides["config/dymint.toml"] = dymintTomlOverrides
-	// Create chain factory with dymension
-	numHubVals := 1
-	numHubFullNodes := 1
-	numRollAppVals := 1
-	numRollAppFn := 1
+// 	configFileOverrides["config/dymint.toml"] = dymintTomlOverrides
+// 	// Create chain factory with dymension
+// 	numHubVals := 1
+// 	numHubFullNodes := 1
+// 	numRollAppVals := 1
+// 	numRollAppFn := 1
 
-	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
-		{
-			Name: "rollapp1",
-			ChainConfig: ibc.ChainConfig{
-				Type:                "rollapp-dym",
-				Name:                "rollapp-temp",
-				ChainID:             "rollappwasm_1234-1",
-				Images:              []ibc.DockerImage{rollappWasmImage},
-				Bin:                 "rollappd",
-				Bech32Prefix:        "rol",
-				Denom:               "urax",
-				CoinType:            "118",
-				GasPrices:           "0.0urax",
-				GasAdjustment:       1.1,
-				TrustingPeriod:      "112h",
-				EncodingConfig:      encodingConfig(),
-				NoHostMount:         false,
-				ModifyGenesis:       nil,
-				ConfigFileOverrides: configFileOverrides,
-			},
-			NumValidators: &numRollAppVals,
-			NumFullNodes:  &numRollAppFn,
-		},
-		{
-			Name:          "dymension-hub",
-			ChainConfig:   customEpochConfig("5s"),
-			NumValidators: &numHubVals,
-			NumFullNodes:  &numHubFullNodes,
-		},
-	})
+// 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
+// 		{
+// 			Name: "rollapp1",
+// 			ChainConfig: ibc.ChainConfig{
+// 				Type:                "rollapp-dym",
+// 				Name:                "rollapp-temp",
+// 				ChainID:             "rollappwasm_1234-1",
+// 				Images:              []ibc.DockerImage{rollappWasmImage},
+// 				Bin:                 "rollappd",
+// 				Bech32Prefix:        "rol",
+// 				Denom:               "urax",
+// 				CoinType:            "118",
+// 				GasPrices:           "0.0urax",
+// 				GasAdjustment:       1.1,
+// 				TrustingPeriod:      "112h",
+// 				EncodingConfig:      encodingConfig(),
+// 				NoHostMount:         false,
+// 				ModifyGenesis:       nil,
+// 				ConfigFileOverrides: configFileOverrides,
+// 			},
+// 			NumValidators: &numRollAppVals,
+// 			NumFullNodes:  &numRollAppFn,
+// 		},
+// 		{
+// 			Name:          "dymension-hub",
+// 			ChainConfig:   customEpochConfig("5s"),
+// 			NumValidators: &numHubVals,
+// 			NumFullNodes:  &numHubFullNodes,
+// 		},
+// 	})
 
-	// Get chains from the chain factory
-	chains, err := cf.Chains(t.Name())
-	require.NoError(t, err)
+// 	// Get chains from the chain factory
+// 	chains, err := cf.Chains(t.Name())
+// 	require.NoError(t, err)
 
-	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
-	dymension := chains[1].(*dym_hub.DymHub)
+// 	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
+// 	dymension := chains[1].(*dym_hub.DymHub)
 
-	// Relayer Factory
-	client, network := test.DockerSetup(t)
+// 	// Relayer Factory
+// 	client, network := test.DockerSetup(t)
 
-	ic := test.NewSetup().
-		AddRollUp(dymension, rollapp1)
+// 	ic := test.NewSetup().
+// 		AddRollUp(dymension, rollapp1)
 
-	rep := testreporter.NewNopReporter()
-	eRep := rep.RelayerExecReporter(t)
+// 	rep := testreporter.NewNopReporter()
+// 	eRep := rep.RelayerExecReporter(t)
 
-	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
-		TestName:         t.Name(),
-		Client:           client,
-		NetworkID:        network,
-		SkipPathCreation: true,
+// 	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
+// 		TestName:         t.Name(),
+// 		Client:           client,
+// 		NetworkID:        network,
+// 		SkipPathCreation: true,
 
-		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
-		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
-	}, nil, "", nil)
-	require.NoError(t, err)
+// 		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
+// 		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
+// 	}, nil, "", nil)
+// 	require.NoError(t, err)
 
-	// Wait for rollapp finalized
-	rollapp1Height, err := rollapp1.Height(ctx)
-	require.NoError(t, err)
-	dymension.WaitUntilRollappHeightIsFinalized(ctx, rollapp1.GetChainID(), rollapp1Height, 300)
+// 	// Wait for rollapp finalized
+// 	rollapp1Height, err := rollapp1.Height(ctx)
+// 	require.NoError(t, err)
+// 	dymension.WaitUntilRollappHeightIsFinalized(ctx, rollapp1.GetChainID(), rollapp1Height, 300)
 
-	// Stop the full node
-	err = rollapp1.FullNodes[0].StopContainer(ctx)
-	require.NoError(t, err)
+// 	// Stop the full node
+// 	err = rollapp1.FullNodes[0].StopContainer(ctx)
+// 	require.NoError(t, err)
 
-	// Wait for a few blocks before start the node again and sync
-	err = testutil.WaitForBlocks(ctx, 50, rollapp1)
-	require.NoError(t, err)
+// 	// Wait for a few blocks before start the node again and sync
+// 	err = testutil.WaitForBlocks(ctx, 50, rollapp1)
+// 	require.NoError(t, err)
 
-	// Start full node again
-	err = rollapp1.FullNodes[0].StartContainer(ctx)
-	require.NoError(t, err)
+// 	// Start full node again
+// 	err = rollapp1.FullNodes[0].StartContainer(ctx)
+// 	require.NoError(t, err)
 
-	// Poll until full node is sync
-	err = testutil.WaitForCondition(
-		time.Minute*10,
-		time.Second*5, // each epoch is 5 seconds
-		func() (bool, error) {
-			valHeight, err := rollapp1.Validators[0].Height(ctx)
-			require.NoError(t, err)
+// 	// Poll until full node is sync
+// 	err = testutil.WaitForCondition(
+// 		time.Minute*10,
+// 		time.Second*5, // each epoch is 5 seconds
+// 		func() (bool, error) {
+// 			valHeight, err := rollapp1.Validators[0].Height(ctx)
+// 			require.NoError(t, err)
 
-			fullnodeHeight, err := rollapp1.FullNodes[0].Height(ctx)
-			require.NoError(t, err)
+// 			fullnodeHeight, err := rollapp1.FullNodes[0].Height(ctx)
+// 			require.NoError(t, err)
 
-			if valHeight > fullnodeHeight {
-				return false, nil
-			}
+// 			if valHeight > fullnodeHeight {
+// 				return false, nil
+// 			}
 
-			return true, nil
-		},
-	)
-	require.NoError(t, err)
-}
+// 			return true, nil
+// 		},
+// 	)
+// 	require.NoError(t, err)
+// }
 
 // TestFullnodeSync_Celestia_EVM tests the synchronization of a fullnode using Celestia as DA.
 func TestFullnodeSync_Celestia_EVM(t *testing.T) {
@@ -329,8 +329,13 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 	nodeStore := "/home/celestia/light"
 	p2pNetwork := "mocha-4"
 	coreIp := "celestia-testnet-consensus.itrocket.net"
-	trustedHash := "\"A62DD37EDF3DFF5A7383C6B5AA2AC619D1F8C8FEB7BA07730E50BBAAC8F2FF0C\""
-	sampleFrom := 2395649
+	// trustedHash := "\"A62DD37EDF3DFF5A7383C6B5AA2AC619D1F8C8FEB7BA07730E50BBAAC8F2FF0C\""
+	// sampleFrom := 2395649
+
+	url := "https://api-mocha.celenium.io/v1/block/count"
+	headerKey := "User-Agent"
+	headerValue := "Apidog/1.0.0 (https://apidog.com)"
+	rpcEndpoint := "http://celestia-testnet-consensus.itrocket.net:26657"
 
 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
 		{
@@ -346,7 +351,7 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 				Images: []ibc.DockerImage{
 					{
 						Repository: "ghcr.io/decentrio/light",
-						Version:    "debug",
+						Version:    "latest",
 						UidGid:     "1025:1025",
 					},
 				},
@@ -386,11 +391,11 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 	}, nil, "", nil)
 	// require.NoError(t, err)
 
-	validator, err := celestia.GetNode().AccountKeyBech32(ctx, "validator")
+	validator, err := celestia.Validators[0].AccountKeyBech32(ctx, "validator")
 	require.NoError(t, err)
 
 	// Get fund for submit blob
-	for i := 0; i < 13; i++ {
+	for i := 0; i < 11; i++ {
 		GetFaucet("http://18.184.170.181:3000/api/get-tia", validator)
 		err = testutil.WaitForBlocks(ctx, 8, celestia)
 		require.NoError(t, err)
@@ -402,9 +407,25 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 	err = testutil.WaitForBlocks(ctx, 3, celestia)
 	require.NoError(t, err)
 
+	// Change the file permissions
+	command := []string{"chmod", "-R", "777", "/home/celestia/light/config.toml"}
+
+	_, _, err = celestia.Exec(ctx, command, nil)
+	require.NoError(t, err)
+
 	file, err := os.Open("/tmp/celestia/light/config.toml")
 	require.NoError(t, err)
 	defer file.Close()
+
+	lastestBlockHeight, err := GetLatestBlockHeight(url, headerKey, headerValue)
+	require.NoError(t, err)
+	lastestBlockHeight = strings.TrimRight(lastestBlockHeight, "\n")
+	heightOfBlock, err := strconv.ParseInt(lastestBlockHeight, 10, 64) // base 10, bit size 64
+	require.NoError(t, err)
+
+	hash, err := celestia.GetNode().GetHashOfBlockHeightWithCustomizeRpcEndpoint(ctx, fmt.Sprintf("%d", heightOfBlock-2), rpcEndpoint)
+	require.NoError(t, err)
+	hash = strings.TrimRight(hash, "\n")
 
 	var lines []string
 	scanner := bufio.NewScanner(file)
@@ -414,9 +435,9 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 
 	for i, line := range lines {
 		if strings.HasPrefix(line, "  TrustedHash =") {
-			lines[i] = fmt.Sprintf("  TrustedHash = %s", trustedHash)
+			lines[i] = fmt.Sprintf("  TrustedHash = \"%s\"", hash)
 		} else if strings.HasPrefix(line, "  SampleFrom =") {
-			lines[i] = fmt.Sprintf("  SampleFrom = %d", sampleFrom)
+			lines[i] = fmt.Sprintf("  SampleFrom = %d", heightOfBlock-2)
 		} else if strings.HasPrefix(line, "  Address =") {
 			lines[i] = fmt.Sprintf("  Address = \"0.0.0.0\"")
 		}
@@ -508,19 +529,8 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
 	dymension := chains[1].(*dym_hub.DymHub)
 
-	r := test.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t),
-		relayer.CustomDockerImage(RelayerMainRepo, relayerVersion, "100:1000"), relayer.ImagePull(pullRelayerImage),
-	).Build(t, client, "relayer", network)
-
 	ic = test.NewSetup().
-		AddRollUp(dymension, rollapp1).
-		AddRelayer(r, "relayer").
-		AddLink(test.InterchainLink{
-			Chain1:  dymension,
-			Chain2:  rollapp1,
-			Relayer: r,
-			Path:    ibcPath,
-		})
+		AddRollUp(dymension, rollapp1)
 
 	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
 		TestName:         t.Name(),
