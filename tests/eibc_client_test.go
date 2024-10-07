27f71de6d851aeabae93097c017d38419f45badb
hungdinh82 <hungngohihihi@gmail.com>
2024-10-07 16:25:22 +0700

    fix Test_EIBC_Client_NoFundForToken_EVM

--- tests/eibc_client_test.go
+++ tests/eibc_client_test.go
@@ -1184,6 +1184,9 @@ func Test_EIBC_Client_NoFundForToken_EVM(t *testing.T) {
 	err = r.StartRelayer(ctx, eRep, ibcPath)
 	require.NoError(t, err)
 
+	rollappHeight, err := rollapp1.GetNode().Height(ctx)
+	require.NoError(t, err)
+
 	// Get the IBC denom for urax on Hub
 	rollappTokenDenom := transfertypes.GetPrefixedDenom(channel.Counterparty.PortID, channel.Counterparty.ChannelID, rollapp1.Config().Denom)
 	rollappIBCDenom := transfertypes.ParseDenomTrace(rollappTokenDenom).IBCDenom()
