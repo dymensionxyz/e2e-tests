718e1f6dfa3cdd76eaf8cdb15aedcbf098bb6bc6
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-10-14 23:07:27 +0700

    chore: run clean commands as sudo

--- clean-live.sh
+++ clean-live.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf ~/.dymension/keyring-test/dym1.info
-rm -rf ~/.dymension/keyring-test/dym2.info
-rm -rf ~/.rollapp_evm/keyring-test/rolx1.info 
-rm -rf ~/.rollapp-wasm/keyring-test/roly1.info 
-rm -rf ~/.celestia-app/keyring-test/mocha1.info 
+sudo rm -rf ~/.dymension/keyring-test/dym1.info
+sudo rm -rf ~/.dymension/keyring-test/dym2.info
+sudo rm -rf ~/.rollapp_evm/keyring-test/rolx1.info
+sudo rm -rf ~/.rollapp-wasm/keyring-test/roly1.info
+sudo rm -rf ~/.celestia-app/keyring-test/mocha1.info
--- clean.sh
+++ clean.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-rm -rf /tmp/relayer*
-rm -rf /tmp/rollapp*
-rm -rf /tmp/dym*
-rm -rf /tmp/sequencer*
-rm -rf /tmp/celes*
-rm -rf /tmp/gaia*
-rm -rf /tmp/side*
-rm -rf /tmp/.eibc-client*
-rm -rf /tmp/new_rollapp*
+sudo rm -rf /tmp/relayer*
+sudo rm -rf /tmp/rollapp*
+sudo rm -rf /tmp/dym*
+sudo rm -rf /tmp/sequencer*
+sudo rm -rf /tmp/celes*
+sudo rm -rf /tmp/gaia*
+sudo rm -rf /tmp/side*
+sudo rm -rf /tmp/.eibc-client*
+sudo rm -rf /tmp/new_rollapp*
 echo "Clean success!!"
-
