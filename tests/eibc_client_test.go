17db6c8ce8a73c8dee24a5c7d6cbdfb4152c2d63
anhductn2001 <ntd1312000@gmail.com>
2024-11-21 13:16:45 +0700

    fix eibc-client

--- tests/eibc_client_test.go
+++ tests/eibc_client_test.go
@@ -3,8 +3,10 @@ package tests
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
@@ -34,6 +36,16 @@ import (
 	"github.com/decentrio/rollup-e2e-testing/testreporter"
 	"github.com/decentrio/rollup-e2e-testing/testutil"
 )
+
+type Member struct {
+	Address  string `json:"address"`
+	Weight   string `json:"weight"`
+	Metadata string `json:"metadata"`
+}
+
+type MembersJSON struct {
+	Members []Member `json:"members"`
+}
 
 type Config struct {
 	HomeDir      string             `yaml:"home_dir"`
@@ -186,182 +198,26 @@ func Test_EIBC_Client_Success_EVM(t *testing.T) {
 	dymintTomlOverrides["max_proof_time"] = "500ms"
 	dymintTomlOverrides["batch_submit_time"] = "50s"
 	dymintTomlOverrides["p2p_blocksync_enabled"] = "false"
+	dymintTomlOverrides["da_config"] = "{\"host\":\"grpc-da-container\",\"port\": 7980}"
 
-	configFileOverrides1 := make(map[string]any)
-	configTomlOverrides1 := make(testutil.Toml)
-	configTomlOverrides1["timeout_commit"] = "2s"
-	configTomlOverrides1["timeout_propose"] = "2s"
-	configTomlOverrides1["index_all_keys"] = "true"
-	configTomlOverrides1["mode"] = "validator"
-
-	configFileOverrides1["config/config.toml"] = configTomlOverrides1
+	configFileOverrides := make(map[string]any)
+	configFileOverrides["config/dymint.toml"] = dymintTomlOverrides
 
 	// Create chain factory with dymension
 	modifyEVMGenesisKV := append(
 		rollappEVMGenesisKV,
 		cosmos.GenesisKV{
 			Key:   "app_state.rollappparams.params.da",
-			Value: "celestia",
+			Value: "grpc",
 		},
 	)
 
 	numHubVals := 1
 	numHubFullNodes := 1
-	numCelestiaFn := 0
 	numRollAppFn := 1
 	numRollAppVals := 1
-	nodeStore := "/home/celestia/light"
-	p2pNetwork := "mocha-4"
-	coreIp := "mocha-4-consensus.mesa.newmetric.xyz"
-
-	url := "https://api-mocha.celenium.io/v1/block/count"
-	headerKey := "User-Agent"
-	headerValue := "Apidog/1.0.0 (https://apidog.com)"
-	rpcEndpoint := "http://rpc-mocha.pops.one:26657"
 
 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
-		{
-			Name: "celes-hub",
-			ChainConfig: ibc.ChainConfig{
-				Name:           "celestia",
-				Denom:          "utia",
-				Type:           "hub-celes",
-				GasPrices:      "0.002utia",
-				TrustingPeriod: "112h",
-				ChainID:        "test",
-				Bin:            "celestia-appd",
-				Images: []ibc.DockerImage{
-					{
-						Repository: "ghcr.io/decentrio/light",
-						Version:    "latest",
-						UidGid:     "1025:1025",
-					},
-				},
-				Bech32Prefix:        "celestia",
-				CoinType:            "118",
-				GasAdjustment:       1.5,
-				ConfigFileOverrides: configFileOverrides1,
-			},
-			NumValidators: &numHubVals,
-			NumFullNodes:  &numCelestiaFn,
-		},
-	})
-
-	// Get chains from the chain factory
-	chains, err := cf.Chains(t.Name())
-	require.NoError(t, err)
-
-	celestia := chains[0].(*celes_hub.CelesHub)
-
-	// Relayer Factory
-	client, network := test.DockerSetup(t)
-
-	ic := test.NewSetup().
-		AddChain(celestia)
-
-	rep := testreporter.NewNopReporter()
-	eRep := rep.RelayerExecReporter(t)
-
-	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
-		TestName:         t.Name(),
-		Client:           client,
-		NetworkID:        network,
-		SkipPathCreation: true,
-	}, nil, "", nil, true, 1179360)
-	require.NoError(t, err)
-
-	validator, err := celestia.Validators[0].AccountKeyBech32(ctx, "validator")
-	require.NoError(t, err)
-
-	// Get fund for submit blob
-	GetFaucet("http://18.184.170.181:3000/api/get-tia", validator)
-	err = testutil.WaitForBlocks(ctx, 2, celestia)
-	require.NoError(t, err)
-
-	err = celestia.GetNode().InitCelestiaDaLightNode(ctx, nodeStore, p2pNetwork, nil)
-	require.NoError(t, err)
-
-	err = testutil.WaitForBlocks(ctx, 3, celestia)
-	require.NoError(t, err)
-
-	file, err := os.Open("/tmp/celestia/light/config.toml")
-	require.NoError(t, err)
-	defer file.Close()
-
-	lastestBlockHeight, err := GetLatestBlockHeight(url, headerKey, headerValue)
-	require.NoError(t, err)
-	lastestBlockHeight = strings.TrimRight(lastestBlockHeight, "\n")
-	heightOfBlock, err := strconv.ParseInt(lastestBlockHeight, 10, 64) // base 10, bit size 64
-	require.NoError(t, err)
-
-	hash, err := celestia.GetNode().GetHashOfBlockHeightWithCustomizeRpcEndpoint(ctx, fmt.Sprintf("%d", heightOfBlock-2), rpcEndpoint)
-	require.NoError(t, err)
-
-	hash = strings.TrimRight(hash, "\n")
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	for i, line := range lines {
-		if strings.HasPrefix(line, "  TrustedHash =") {
-			lines[i] = fmt.Sprintf("  TrustedHash = \"%s\"", hash)
-		} else if strings.HasPrefix(line, "  SampleFrom =") {
-			lines[i] = fmt.Sprintf("  SampleFrom = %d", heightOfBlock-2)
-		} else if strings.HasPrefix(line, "  Address =") {
-			lines[i] = fmt.Sprintf("  Address = \"0.0.0.0\"")
-		}
-	}
-
-	output := strings.Join(lines, "\n")
-	file, err = os.Create("/tmp/celestia/light/config.toml")
-	require.NoError(t, err)
-	defer file.Close()
-
-	_, err = file.Write([]byte(output))
-	require.NoError(t, err)
-
-	containerID := fmt.Sprintf("test-val-0-%s", t.Name())
-
-	// Create an exec instance
-	execConfig := types.ExecConfig{
-		Cmd: strslice.StrSlice([]string{"celestia", "light", "start", "--node.store", nodeStore, "--gateway", "--core.ip", coreIp, "--p2p.network", p2pNetwork, "--keyring.keyname", "validator"}), // Replace with your command and arguments
-	}
-
-	execIDResp, err := client.ContainerExecCreate(ctx, containerID, execConfig)
-	if err != nil {
-		fmt.Println("Err:", err)
-	}
-
-	execID := execIDResp.ID
-
-	// Start the exec instance
-	execStartCheck := types.ExecStartCheck{
-		Tty: false,
-	}
-
-	if err := client.ContainerExecStart(ctx, execID, execStartCheck); err != nil {
-		fmt.Println("Err:", err)
-	}
-
-	err = testutil.WaitForBlocks(ctx, 10, celestia)
-	require.NoError(t, err)
-
-	celestia_token, err := celestia.GetNode().GetAuthTokenCelestiaDaLight(ctx, p2pNetwork, nodeStore)
-	require.NoError(t, err)
-	println("check token: ", celestia_token)
-	celestia_namespace_id, err := RandomHex(10)
-	require.NoError(t, err)
-	println("check namespace: ", celestia_namespace_id)
-	da_config := fmt.Sprintf("{\"base_url\": \"http://test-val-0-%s:26658\", \"timeout\": 60000000000, \"gas_prices\":1.0, \"gas_adjustment\": 1.3, \"namespace_id\": \"%s\", \"auth_token\":\"%s\"}", t.Name(), celestia_namespace_id, celestia_token)
-
-	configFileOverrides := make(map[string]any)
-	dymintTomlOverrides["namespace_id"] = celestia_namespace_id
-	dymintTomlOverrides["da_config"] = da_config
-	configFileOverrides["config/dymint.toml"] = dymintTomlOverrides
-
-	cf = test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
 		{
 			Name: "rollapp1",
 			ChainConfig: ibc.ChainConfig{
@@ -420,18 +276,21 @@ func Test_EIBC_Client_Success_EVM(t *testing.T) {
 	})
 
 	// Get chains from the chain factory
-	chains, err = cf.Chains(t.Name())
+	chains, err := cf.Chains(t.Name())
 	require.NoError(t, err)
 
 	rollapp1 := chains[0].(*dym_rollapp.DymRollApp)
 	dymension := chains[1].(*dym_hub.DymHub)
 
 	// Relayer Factory
+	client, network := test.DockerSetup(t)
+	StartDA(ctx, t, client, network)
+
 	r := test.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t),
 		relayer.CustomDockerImage(RelayerMainRepo, relayerVersion, "100:1000"), relayer.ImagePull(pullRelayerImage),
 	).Build(t, client, "relayer", network)
 
-	ic = test.NewSetup().
+	ic := test.NewSetup().
 		AddRollUp(dymension, rollapp1).
 		AddRelayer(r, "relayer").
 		AddLink(test.InterchainLink{
@@ -441,6 +300,9 @@ func Test_EIBC_Client_Success_EVM(t *testing.T) {
 			Path:    ibcPath,
 		})
 
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
 	err = ic.Build(ctx, eRep, test.InterchainBuildOptions{
 		TestName:         t.Name(),
 		Client:           client,
@@ -449,7 +311,76 @@ func Test_EIBC_Client_Success_EVM(t *testing.T) {
 
 		// This can be used to write to the block database which will index all block data e.g. txs, msgs, events, etc.
 		// BlockDatabaseFile: test.DefaultBlockDatabaseFilepath(),
-	}, nil, "", nil, false, 1179360)
+	}, nil, "", nil, true, 1179360)
+	require.NoError(t, err)
+
+	containerID := fmt.Sprintf("ra-rollappevm_1234-1-val-0-%s", t.Name())
+
+	// Get the container details
+	containerJSON, err := client.ContainerInspect(context.Background(), containerID)
+	require.NoError(t, err)
+
+	// Extract the IP address from the network settings
+	// If the container is using a custom network, the IP might be under a specific network name
+	var ipAddress string
+	for _, network := range containerJSON.NetworkSettings.Networks {
+		ipAddress = network.IPAddress
+		break // Assuming we only need the IP from the first network
+	}
+
+	nodeId, err := rollapp1.Validators[0].GetNodeId(ctx)
+	require.NoError(t, err)
+	nodeId = strings.TrimRight(nodeId, "\n")
+	p2p_bootstrap_node := fmt.Sprintf("/ip4/%s/tcp/26656/p2p/%s", ipAddress, nodeId)
+
+	rollapp1HomeDir := strings.Split(rollapp1.FullNodes[0].HomeDir(), "/")
+	rollapp1FolderName := rollapp1HomeDir[len(rollapp1HomeDir)-1]
+
+	file, err := os.Open(fmt.Sprintf("/tmp/%s/config/dymint.toml", rollapp1FolderName))
+	require.NoError(t, err)
+	defer file.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for i, line := range lines {
+		if strings.HasPrefix(line, "p2p_bootstrap_nodes =") {
+			lines[i] = fmt.Sprintf("p2p_bootstrap_nodes = \"%s\"", p2p_bootstrap_node)
+		}
+	}
+
+	output := strings.Join(lines, "\n")
+	file, err = os.Create(fmt.Sprintf("/tmp/%s/config/dymint.toml", rollapp1FolderName))
+	require.NoError(t, err)
+	defer file.Close()
+
+	_, err = file.Write([]byte(output))
+	require.NoError(t, err)
+
+	// Start full node
+	err = rollapp1.FullNodes[0].StopContainer(ctx)
+	require.NoError(t, err)
+
+	err = rollapp1.FullNodes[0].StartContainer(ctx)
+	require.NoError(t, err)
+
+	addrDym, _ := r.GetWallet(dymension.GetChainID())
+	err = dymension.GetNode().SendFunds(ctx, "faucet", ibc.WalletData{
+		Address: addrDym.FormattedAddress(),
+		Amount:  math.NewInt(10_000_000_000_000),
+		Denom:   dymension.Config().Denom,
+	})
+	require.NoError(t, err)
+
+	addrRA, _ := r.GetWallet(rollapp1.GetChainID())
+	err = rollapp1.GetNode().SendFunds(ctx, "faucet", ibc.WalletData{
+		Address: addrRA.FormattedAddress(),
+		Amount:  math.NewInt(10_000_000_000_000),
+		Denom:   rollapp1.Config().Denom,
+	})
 	require.NoError(t, err)
 
 	CreateChannel(ctx, t, r, eRep, dymension.CosmosChain, rollapp1.CosmosChain, ibcPath)
@@ -523,6 +454,24 @@ func Test_EIBC_Client_Success_EVM(t *testing.T) {
 	testutil.AssertBalance(t, ctx, dymension, dymensionUserAddr, rollappIBCDenom, transferData.Amount.Sub(bigBridgingFee))
 
 	StartDB(ctx, t, client, network)
+
+	membersData, err := ioutil.ReadFile("./data/members.json")
+	require.NoError(t, err)
+
+	var members MembersJSON
+	err = json.Unmarshal(membersData, &members)
+	require.NoError(t, err)
+
+	newAddress := dymensionUserAddr
+	for i := range members.Members {
+		members.Members[i].Address = newAddress
+	}
+
+	updatedJSON, err := json.MarshalIndent(members, "", "  ")
+	require.NoError(t, err)
+
+	err = ioutil.WriteFile("./data/members.json", updatedJSON, 0755)
+	require.NoError(t, err)
 
 	txHash, err := dymension.GetNode().CreateGroup(ctx, dymensionUser.KeyName(), "==A", "members.json")
 	fmt.Println(txHash)
