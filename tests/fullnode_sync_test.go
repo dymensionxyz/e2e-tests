237aa8f687b61ee1c0608c1eca48e05dea60d75f
Sergi Rene <sergi@dymension.xyz>
2025-12-19 13:59:11 +0100

    ceelestia endpoint replaceg

--- tests/fullnode_sync_test.go
+++ tests/fullnode_sync_test.go
@@ -526,7 +526,7 @@ func TestFullnodeSync_Celestia_EVM(t *testing.T) {
 	url := "https://api-mocha.celenium.io/v1/block/count"
 	headerKey := "User-Agent"
 	headerValue := "Apidog/1.0.0 (https://apidog.com)"
-	rpcEndpoint := "http://celestia-mocha-archive-rpc.mzonder.com:26657"
+	rpcEndpoint := "https://rpc-mocha.pops.one:443"
 
 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
 		{
@@ -780,7 +780,7 @@ func TestFullnodeSync_Celestia_Wasm(t *testing.T) {
 	url := "https://api-mocha.celenium.io/v1/block/count"
 	headerKey := "User-Agent"
 	headerValue := "Apidog/1.0.0 (https://apidog.com)"
-	rpcEndpoint := "http://celestia-mocha-archive-rpc.mzonder.com:26657"
+	rpcEndpoint := "https://rpc-mocha.pops.one:443"
 
 	cf := test.NewBuiltinChainFactory(zaptest.NewLogger(t), []*test.ChainSpec{
 		{
